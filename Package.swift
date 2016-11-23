00b54add653c1b993a37bc815a752e0668b8e73b
Aleksey Mashanov <a.mashanov@corp.mail.ru>
2016-11-23 15:45:27 +0300

    Use clone url in dependencies

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@ import PackageDescription
 let package = Package(
 	name: "OctopusBox",
 	dependencies: [
-		.Package(url: "https://github.com/my-mail-ru/swift-BinaryEncoding", majorVersion: 0),
-		.Package(url: "https://github.com/my-mail-ru/swift-IProto", majorVersion: 0),
-		.Package(url: "https://github.com/my-mail-ru/swift-Octopus", majorVersion: 0),
+		.Package(url: "https://github.com/my-mail-ru/swift-BinaryEncoding.git", majorVersion: 0),
+		.Package(url: "https://github.com/my-mail-ru/swift-IProto.git", majorVersion: 0),
+		.Package(url: "https://github.com/my-mail-ru/swift-Octopus.git", majorVersion: 0),
 	]
 )
